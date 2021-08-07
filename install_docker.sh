b9f2a7ceec3c0726dc5f666239e8c720b44d7727
Markus Neugebauer <markus@newgefarmer.de>
2021-08-07 21:16:20 +0200

    Update output

--- install_docker.sh
+++ install_docker.sh
@@ -5,7 +5,7 @@
 GREEN='\033[1;32m'
 NC='\033[0m' # No Color
  
-printf "${GREEN}\nInstallation of Docker and Docker-Compose\n\nUpdate the apt package index and install packages to allow apt to use a repository over HTTPS${NC}\n"
+printf "${GREEN}\n-- Installation of Docker and Docker-Compose\n\nUpdate the apt package index and install packages to allow apt to use a repository over HTTPS${NC}\n\n"
 sudo apt-get update
  
 sudo apt-get install \
@@ -16,7 +16,7 @@ sudo apt-get install \
     software-properties-common \
     -y
  
-printf "${GREEN} -- Add Docker’s official GPG key${NC}\n"
+printf "${GREEN}-- Add Docker’s official GPG key${NC}\n"
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
  
 printf "${GREEN} -- Set up the stable repository${NC}\n"
@@ -25,27 +25,27 @@ sudo add-apt-repository \
    $(lsb_release -cs) \
    stable"#
  
-printf "${GREEN} -- Install the latest version of Docker Engine and containerd${NC}\n"
+printf "${GREEN}-- Install the latest version of Docker Engine and containerd${NC}\n"
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
  
-printf "${GREEN} -- Configure Docker to start on boot${NC}\n"
+printf "${GREEN}-- Configure Docker to start on boot${NC}\n"
 sudo systemctl start docker
 sudo systemctl enable docker
  
-printf "${GREEN} -- Create the docker group${NC}\n"
+printf "${GREEN}-- Create the docker group${NC}\n"
 sudo groupadd docker
  
-printf "${GREEN} -- Add current user to the docker group${NC}\n"
+printf "${GREEN}-- Add current user to the docker group${NC}\n"
 sudo usermod -aG docker $USER
  
-printf "${GREEN}\n\nInstallation Docker-Compose V1.29.2 from Github${NC}\n"
+printf "${GREEN}\n\n-- Installation Docker-Compose V1.29.2 from Github${NC}\n"
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
  
-printf "${GREEN}\n -- Verify installation of docker-compose${NC}\n"
+printf "${GREEN}\n-- Verify installation of docker-compose${NC}\n"
 docker-compose --version
  
 # Verify that you can run docker commands without sudo
-printf "${GREEN}\n -- Done!\n\nCheck installation of docker with command: 'docker run hello-world'\n"
+printf "${GREEN}\n-- Done!\n\nCheck installation of docker with command: 'docker run hello-world'\n"
 printf "On permission errors try to reboot${NC}\n\n"
