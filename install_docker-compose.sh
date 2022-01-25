5265f32cac015be42117f4e4d10139bf5deb84b0
Markus Neugebauer <markus@newgefarmer.de>
2022-01-25 20:58:15 +0100

    DockerCompose: Update to use URL from Github-API-JSON

--- install_docker-compose.sh
+++ install_docker-compose.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
  
 # Download and run script: wget -O - https://raw.githubusercontent.com/MadMax411/install-software/master/install_docker-compose.sh | bash
- 
+
 GREEN='\033[1;32m'
 NC='\033[0m' # No Color
 
 VERSION_DOCKER_COMPOSE=$(curl -s -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/docker/compose/releases/latest | jq -r .tag_name)
+MACHINE=$(uname -s)
+ARCH=$(uname -m)
+DOWNLOAD_URL=$(curl -s -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/docker/compose/releases/latest | jq -r '.assets[] | select(.name == "docker-compose-'${MACHINE,,}'-'$ARCH'").browser_download_url')
 
-printf "${GREEN}\n\n-- Installation Docker-Compose ${VERSION_DOCKER_COMPOSE} from Github${NC}\n"
-sudo curl -L "https://github.com/docker/compose/releases/download/${VERSION_DOCKER_COMPOSE}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+printf "${GREEN}\n-- Installation Docker-Compose ${VERSION_DOCKER_COMPOSE} from ${DOWNLOAD_URL}...${NC}\n"
+
+#OLD: sudo curl -L "https://github.com/docker/compose/releases/download/${VERSION_DOCKER_COMPOSE}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L ${DOWNLOAD_URL} -o /usr/local/bin/docker-compose
+
 sudo chmod +x /usr/local/bin/docker-compose
  
 printf "${GREEN}\n-- Verify installation of docker-compose${NC}\n"
