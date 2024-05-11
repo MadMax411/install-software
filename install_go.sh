3bd97c1fe06a84a9f416111b05d545a6f09f33ce
Markus Neugebauer <markus@newgefarmer.de>
2024-05-11 21:03:47 +0200

    Update Go: Check for local version and install only if version is newer

--- install_go.sh
+++ install_go.sh
@@ -4,22 +4,36 @@ NC='\033[0m' # No Color
 
 CONTENT=$(curl -s https://go.dev/VERSION?m=text) 
 VERSION=$(echo $CONTENT | cut -d " " -f 1)
+INSTALLED_VERSION="goNone"
 INSTALLDIR=/usr/local/go
 FILE=$VERSION.linux-amd64.tar.gz
 URL=https://go.dev/dl/$FILE
 
-echo -e "${GREEN}\n>>> Downloading Go ${VERSION:2}...${NC}"
+# Check if go exsits and check if newest version is installed
+if [ -f "$(which go)" ]; then
+    INSTALLED_VERSION=$(go version | cut -d " " -f 3)
+    if [ "$INSTALLED_VERSION" = "$VERSION" ]; then
+        echo -e "${GREEN}Newest version ${VERSION:2} of Go is already installed${NC} 🙂"
+        exit 0
+    fi
+fi
+
+# Download the new version
+echo -e "${GREEN}\n>>> Current Version ${INSTALLED_VERSION:2} -> Downloading newer version ${VERSION:2}..."
 wget $URL
  
+# Remove the old installation directory
 if [ -d "$INSTALLDIR" ]; then
     echo -e "${GREEN}>>> Remove old Go version...${NC}"
     sudo rm -rf $INSTALLDIR
 fi
 
+# Extract the the version of Go
 echo -e "${GREEN}>>> Extract go tar-package...${NC}"
 sudo tar -C /usr/local -xzf $FILE
 rm $FILE > /dev/null
 
+# Add the install directory to path env
 if [[ ! "$PATH" == *"$INSTALLDIR/bin"* ]]; then
     echo -e "${GREEN}>>> Set ENV variables for Go...${NC}"
     echo "" >> ~/.bashrc
