7d44466c4839e6609b05a7d48f9062b7a7dc449e
Markus Neugebauer <markus@newgefarmer.de>
2024-08-19 10:28:38 +0200

    Docker-Compose: Check installed version

--- install_docker-compose.sh
+++ install_docker-compose.sh
@@ -10,6 +10,15 @@ MACHINE=$(uname -s)
 ARCH=$(uname -m)
 DOWNLOAD_URL=$(curl -s -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/docker/compose/releases/latest | jq -r '.assets[] | select(.name == "docker-compose-'${MACHINE,,}'-'$ARCH'").browser_download_url')
 
+# Check if docker-compose exsits and check if newest version is installed
+if [ -f "$(which docker-compose)" ]; then
+    INSTALLED_VERSION=$(docker-compose --version | cut -d " " -f 4)
+    if [ "$INSTALLED_VERSION" = "$VERSION_DOCKER_COMPOSE" ]; then
+        echo -e "${GREEN}Newest version ${VERSION_DOCKER_COMPOSE:1} of Docker-Compose is already installed${NC} 🙂"
+        exit 0
+    fi
+fi
+
 printf "${GREEN}\n-- Installation Docker-Compose ${VERSION_DOCKER_COMPOSE} from ${DOWNLOAD_URL}...${NC}\n"
 
 #OLD: sudo curl -L "https://github.com/docker/compose/releases/download/${VERSION_DOCKER_COMPOSE}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
